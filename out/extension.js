16a3efc00c44c9659c461de8fbe003a4c3a0eea5
Zamiell <j.nesta@gmail.com>
2019-02-06 00:50:02 -0500

    initial version with just enums.lua

--- out/extension.js
+++ out/extension.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const fs = require('fs');
+const path = require('path');
+const enumsPath = path.join(__dirname, '..', 'generate-autocomplete-entries', 'enums.json');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
-    const enums = [
-        'ModCallbacks',
-        'ModCallbacks.MC_NPC_UPDATE',
-        'ModCallbacks.MC_POST_UPDATE',
-    ];
+    let enums = fs.readFileSync(enumsPath, 'utf8');
+    enums = JSON.parse(enums);
     const completionArray = [];
     for (const e of enums) {
         completionArray.push(new vscode.CompletionItem(e));
--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,16 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const fs = require('fs');
+const path = require('path');
+
+const enumsPath = path.join(__dirname, '..', 'generate-autocomplete-entries', 'enums.json');
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const enums = [
-		'ModCallbacks',
-		'ModCallbacks.MC_NPC_UPDATE',
-		'ModCallbacks.MC_POST_UPDATE',
-	];
+	let enums = fs.readFileSync(enumsPath, 'utf8');
+	enums = JSON.parse(enums);
 	const completionArray: Array<vscode.CompletionItem> = [];
 	for (const e of enums) {
 		completionArray.push(new vscode.CompletionItem(e));
